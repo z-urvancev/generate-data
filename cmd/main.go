32af0a191e3273df623a05b670c27b1f0914a969
Zuhropenchik <zahvinar358@gmail.com>
2022-12-21 02:37:39 +0300

    fix sync

--- cmd/main.go
+++ cmd/main.go
@@ -11,10 +11,12 @@ func main() {
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func(i int, wg *sync.WaitGroup) {
+			defer wg.Done()
 			cookie := queries.SignUp(dictionary.GenerateEmployer(i))
 			for j := 0; j < 10; j++ {
 				queries.CreateVacancy(dictionary.GenerateVacancy(), cookie)
 			}
 		}(i, &wg)
+		wg.Wait()
 	}
 }
