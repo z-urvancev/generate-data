bdd5e020682f18a400a412b6d1a22ae41fe9e195
Zuhropenchik <zahvinar358@gmail.com>
2022-12-21 23:16:03 +0300

    fixes

--- dictionary/applicant.go
+++ dictionary/applicant.go
@@ -17,10 +17,18 @@ func GenerateApplicant(i int) *models.UserAccount {
 		Location:         generateLocation(),
 		Age:              uint(rand.Int())%34 + 16,
 		DateOfBirth:      time.Date(1970+rand.Int()%36, time.April, rand.Int()%365, 0, 0, 0, 0, time.Local),
+		ContactNumber:    generatePhone(),
 	}
 	return result
 }
 
+func generatePhone() string {
+	a := rand.Int() % 1000
+	d := rand.Int() % 1000
+	b := rand.Int() % 100
+	c := rand.Int() % 100
+	return fmt.Sprintf("+7 (%d) %d-%d-%d", d, a, b, c)
+}
 func generateName() string {
 	i := rand.Int() % len(names)
 	return names[i]
--- dictionary/company.go
+++ dictionary/company.go
@@ -16,6 +16,7 @@ func GenerateEmployer(i int) *models.UserAccount {
 		Location:     generateLocation(),
 		CompanySize:  uint(rand.Int()%100*100 + 100),
 		BusinessType: businessTypes[rand.Int()%len(businessTypes)],
+		Description:  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt posuere ex non tempor. Sed massa erat, venenatis id mattis non, mollis ut nunc. Phasellus fringilla accumsan nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut dapibus sapien eget efficitur rutrum. Suspendisse accumsan bibendum ante id gravida. Pellentesque luctus porta magna quis iaculis. Proin posuere a velit quis viverra. Sed arcu nisl, faucibus a lacinia vel, iaculis a arcu. Etiam a leo id sem tincidunt congue eget a odio.",
 	}
 	return result
 }
